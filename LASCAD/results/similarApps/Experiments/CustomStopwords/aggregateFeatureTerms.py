26a2a3b2054f9052da3ec7615535209a8738b176
RaulMedeiros <raul.medeiros@ehu.eus>
2020-09-18 13:14:52 +0200

    SimilarApps experiments

---
+++ LASCAD/results/similarApps/Experiments/CustomStopwords/aggregateFeatureTerms.py
@@ -0,0 +1,13 @@
+
+
+from pathlib import Path
+f = open("aggregatedTerms.txt", "w")
+for elem in Path('../../../../showcases_out/').rglob('*.*'):
+    f2 = open(elem,"r")
+    data=data=f2.read()
+    data=data.replace('b\'','')
+    data=data.replace('\'','')
+    if(data!='\n'):
+        f.write(data)
+    f2.close()
+f.close()    
---
+++ LASCAD/results/similarApps/Experiments/CustomStopwords/changeCalculator.py
@@ -0,0 +1,39 @@
+import csv
+import pprint
+import re
+import csv
+import sys
+import os
+
+f1 = file('withoutStpWrd.csv', 'r')
+f2 = file('withStpWrd.csv', 'r')
+
+c1 = csv.reader(f1, delimiter=',')
+c2 = csv.reader(f2, delimiter=',')
+master_list = list(c2)
+c1.next()
+row_num=1
+changes=0
+pattern = r'\'(.*?)\''
+changes_list={}
+for row in c1:
+    row_changes=
+    #Calculates the changes for the Top 5 siblings in the matrix
+    for i in range(1, 6):
+        #Clean the data and get only the feature name from the cell
+        row_col= re.search(pattern,row[i]).group(1)
+        master_col= re.search(pattern,master_list[row_num][i]).group(1)
+        if(master_col!=row_col):
+            changes=changes+1
+            row_changes=row_changes+1
+    row_num = row_num+1
+    changes_list[row[0]]= row_changes
+features_changes = 0
+for x in changes_list.values():
+    if(x!=0):
+        features_changes=features_changes+1
+changes_porcentage= features_changes*100/len(changes_list)
+changes_list['Total']=changes
+print('A ' +str(changes_porcentage) + '% of the features suffered some changes\n' )
+pp=pprint.PrettyPrinter(indent=4)
+pp.pprint(changes_list)
